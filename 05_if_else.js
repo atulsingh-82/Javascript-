c7ed2ebbacc73dcd02bc68e66fa628c2d0c57b50
Atul Prasad <atulsingh55199@gmail.com>
2026-06-18 13:11:45 +0530

    Add files via upload

---
+++ 05_if_else.js
@@ -0,0 +1,73 @@
+
+// if Statement 
+
+let age = 18;
+
+if ( age >= 18){
+    console.log ("adult");
+}
+
+// if, else, Statement 
+
+ let age2 = 1
+
+ if(age2 >= 18){
+    console.log("you can vote");
+ }
+ else(
+    console.log("you can not vote")
+ )
+ //Odd And Even No Check
+
+ let num = 54643353
+ if(num%2 ===0){
+    console.log(num," is Even");
+ }
+ else(
+    console.log(num, "is odd")
+)
+
+ // else if, ladder ( multiple condition)
+
+ let marks = 49;
+
+ if(marks >= 80){
+    console.log("grade A+");
+ }
+else if (marks >=70){
+    console.log("grade A");
+}
+else if(marks >= 60){
+    console.log("grade B");
+}
+else if(marks >= 50){
+    console.log("grade C");
+}
+else if(marks < 50){
+    console.log("fail");
+}
+
+// Ternary Operators
+   age = 3;
+   let Result = age >=18?"adult":"not adult"
+   console.log(Result)
+
+   marks = 33
+   let result = marks >=33 ? "pass":"fail"
+   console.log(result)
+
+//    // Practics Questions
+
+    let no = 46
+
+    if (no % 5 === 0)
+{
+    console.log(no,"Number is Devisible by 5");
+}
+    else (
+        console.log(no,"Number is not devide by 5")
+)
+    
+       
+        
+
