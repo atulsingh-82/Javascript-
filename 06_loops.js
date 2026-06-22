d35159d4661f13834d7331d032a5ac2e89cc1228
Atul Prasad <atulsingh55199@gmail.com>
2026-06-22 11:08:23 +0530

    Add files via upload

---
+++ 06_loops.js
@@ -0,0 +1,35 @@
+/*loops are use to repate and repate*/
+//for loops
+
+let i=1;
+for(let i =1;i<=2;i++){
+    console.log("atul singh");
+}
+let a = 7;
+for(let a =7;a <= 7; a++){
+    console.log("hlo");
+}
+let atul=20;
+for(let atul=20;atul<=21;atul++){
+    console.log("yes")
+}
+console.log("loop has end")
+
+/*Calculate sum fron 1 to n*/
+let sum =0;
+for(let i=1;i<=15;i++){
+    sum=sum+i;
+}
+    console.log("sum=",sum);
+
+// while loops
+let k=1;
+while(k <= 10);{
+    console.log("singh is king")
+    k++;
+}
+//Do While loop
+let x=5
+do{console.log("singh");
+    x++;
+}while(x <=6);
