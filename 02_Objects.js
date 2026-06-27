41d7529e21138d51bb0f9b54525e7a6c2f8a61c5
Atul Prasad <atulsingh55199@gmail.com>
2026-06-27 10:12:27 +0530

    Add files via upload

---
+++ 02_Objects.js
@@ -0,0 +1,23 @@
+// Objects is a special data type which store multiple values in key pairs
+
+let student = {
+    name:"atul singh",
+    city:"kushinagar",
+    age: 20,
+
+}
+console.log(student)
+// name,age,city = key proprety
+// atul,kushinagar,20 = value proper
+
+//value access karna
+const user={
+    name:"atul",
+    title:"singh",
+    age:"20",
+}
+console.log(user.name);
+
+//symbol in objects
+const mysum = Symbol("key1")
+
