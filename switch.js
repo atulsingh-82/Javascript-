2877bd05df70e823ab0f8269320f5aad97ab9779
Atul Prasad <atulsingh55199@gmail.com>
2026-07-06 10:08:49 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,53 @@
+// +++++++  SWITCH  ++++++++ //
+
+const month = 4; // agr num ke jagah string value ho to case ke baad string value hi hoga
+switch (month) {
+    case 1:
+        console.log("january")
+         break; /* break use hota h line break krne ke ley agr break hta
+          deya jay to usse case ke baad wala sara console print hoga*/
+}
+switch (month) {
+    case 2:
+        console.log("Feb")
+         break;
+}
+switch (month) {
+    case 3:
+        console.log("March")
+         break;
+}
+switch (month) {
+    case 4:
+        console.log("April")
+         break;
+
+
+    default:
+        break;
+}
+// ++++++++++++++++++++++++++++++++++++++++++++++ //
+const day = "wed";
+switch (day) {
+    case "monday":
+        console.log("MONDAY")
+         break;
+}
+switch (day) {
+    case "tue":
+        console.log("TUESDAY")
+         break;
+}
+switch (day) {
+    case "wed":
+        console.log("WEDNESDAY")
+         break;
+}
+switch (day) {
+    case "thus":
+        console.log("THUSDAY")
+         break;
+
+    default:
+        break;
+}
